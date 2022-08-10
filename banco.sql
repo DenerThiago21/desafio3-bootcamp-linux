9e0fa43fb8360bd128dddae8cf7339459c256816
DenerThiago21 <denerthi.ibi@gmail.com/>
2022-08-10 08:21:56 -0300

    projeto basico com servidor web php e banco de dados mysql

---
+++ banco.sql
@@ -0,0 +1,7 @@
+CREATE TABLE lista (
+    pessoa_id int,
+    nome varchar(50),
+    sobrenome varchar(50),
+    telefone int,
+    host varchar(100)
+);
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    ini_set("display_errors", 1);
+    header('Content-Type: text/html; charset=iso-8859-1');
+
+    echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+
+    $servername = IP_PUBLICO_SERVER;
+    $username = USER_DB;
+    $password = PASSWORD_DB;
+    $database = DATABASE_NAME;
+
+    // Criar conexão
+    $link = new mysqli($servername, $username, $password, $database);
+
+    /* check connection */
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    $valor_rand1 =  rand(1, 999);
+    $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+    $host_name = gethostname();
+
+    $query = "INSERT INTO dados (pessoa_id, nome, sobrenome, telefone, host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand1', '$host_name')";
+
+
+    if ($link->query($query) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $link->error;
+    }
+
+?>
+</body>
+</html>
